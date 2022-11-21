c5422577a00a742ebd02eabfe9904a061032fb18
Adam Bukowski <bukowski.adam@gmail.com>
2022-11-21 15:14:59 +0100

    feat: add service-worker plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "inline",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   server: {
     port: 3000,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
 });
